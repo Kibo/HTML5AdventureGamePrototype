35665479997e93432940a258bcc6bf8ac533e189
kibo <tomasjurman@gmail.com>
2013-01-20 15:08:32 +0100

    prototype 0.0.6

--- WebContent/game/dialogs/meadow_dialog.js
+++ WebContent/game/dialogs/meadow_dialog.js
@@ -15,5 +15,16 @@ DIALOGS_MEADOW_SCENE = [
 						
 		{id:150, type:"text", actor:"drummer", text:"Thanks you.", 		audio:"testa.mp3",	next:160 },
 		{id:160, type:"text", actor:"gnome", text:"You're welcome.", 	audio:"testa.mp3" },
-						              			      													    				  					      												       
+		
+		
+		// exercises had success		
+		{id:200, type:"text", actor:"gnome", text:"Hi drummer, exercises is done.",	audio:"testa.mp3", next:210 },
+		{id:210, type:"text", actor:"drummer", text:"Well done. Go to castle.", 	audio:"testa.mp3",	next:220 },
+		{id:220, type:"text", actor:"gnome", text:"See you.", 						audio:"testa.mp3" },
+				
+		// exercises had not success
+		{id:300, type:"text", actor:"gnome", text:"Hi drummer. I am sorry.",			audio:"testa.mp3",	next:310 },
+		{id:310, type:"text", actor:"drummer", text:"I will not help you. Go away.", 	audio:"testa.mp3", next:320 },
+		{id:320, type:"text", actor:"gnome", text:"Ooops.",				 				audio:"testa.mp3",}
+										              			      													    				  					      												     
 ] //dialog
